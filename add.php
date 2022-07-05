7ad7b4fd3b122db992fb1c0215a4a2b1b7315998
gfftr <gff.terl@gmail.com>
2022-07-05 21:30:44 +0100

    created database and connected

--- add.php
+++ add.php
@@ -1,5 +1,19 @@
 <?php 
+include 'db_connect.php';
 
-require_once('db_connect.php');
+if(isset($_POST['send'])){
+
+ $name = $_POST['task'];
+
+ $sql = "INSERT INTO tasks (name) values ('$name')"; 
+
+ $val = $db->query($sql);
+
+ if($val){
+   //echo "<h1>Successfully inserted</h1>";
+   header('location: index.php');
+ }
+
+}
 
 ?>
--- index.php
+++ index.php
@@ -1,52 +1,99 @@
-<?php 
+<?php
 
 include 'db_connect.php';
 
+// sql select
+$sql = "SELECT * FROM tasks";
 
-
-if(isset($_POST['btnsave'])){
- $pname = $_POST['txtname'];
- $price = $_POST['txtprice'];
-
- //echo "name ".$pname."</br>"."price ".$price;
-
- if(!empty($pname && $price)){
-    $insert = $pdo->prepare("insert into tbl_product(productname,productprice) values(:name,:price)");
-
-    $insert->bindParam(':name',$pname);
-    $insert->bindParam(':price',$price);
-
-    $insert->execute();
-
-    if($insert->rowCount()){
-      echo 'Insert successful';
-    }else{
-      echo ' Insert faled';
-    }
-
- }else{
-      echo 'Fields are empty ';
- }
-
-}
+$rows = $db->query($sql)
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
- <meta charset="UTF-8">
- <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>Simple Crud</title>
 
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Simple Crud</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
 </head>
+
 <body>
-  <form action="" method="post">
-    <p><input type="text" name="txtname" placeholder="Product name"></p>
-    <p><input type="text" name="txtprice" placeholder="Product price"></p>
-    <p><input type="submit" value="Save" name="btnsave"></p>
-  </form>
+
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+
+  <div class="container">
+    <div class="row" style="margin-top: 70px;">
+
+      <div class="col-md-10 col-md-offset-1">
+        <table class="table">
+          <button type="button" class="btn btn-success" data-target="#exampleModal" data-toggle="modal">Add task</button>
+          <button type="button" class="btn btn-default float-right">Print</button>
+          <!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add Task</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+
+        <form action="add.php" method="post">
+        <div class="form-group">
+            <label for="">Task Name</label>
+            <input type="text" required name="task" class="form-control">
+        </div>
+        <input type="submit" name="send" value="Add Task" class="btn btn-success">
+        </form>
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+       
+      </div>
+    </div>
+  </div>
+</div>
+          <p></p>
+
+
+          <thead>
+            <tr>
+              <th scope="col">ID.</th>
+              <th scope="col">Task</th>
+            </tr>
+          </thead>
+          <tbody> <?php while($row = $rows->fetch_assoc()): ?>
+            <tr>
+             
+              <th><?php echo $row['id'] ?></th>
+              <td class="col-md-10"><?php echo $row['name'] ?></td>
+              <td><a href="" class="btn btn-success">Edit</a></td>
+              <td><a href="" class="btn btn-danger">Delete</a></td>
+              
+            </tr> <?php endwhile ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
+
+
+
 </body>
+
 </html>
